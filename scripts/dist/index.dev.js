b7ebfeda579a48d79d7ae5513989e6705e1f897e
marwaoneis <marwa.oneis22@gmail.com>
2023-11-16 23:30:52 +0200

    clone half of the project

--- scripts/dist/index.dev.js
+++ scripts/dist/index.dev.js
@@ -1,40 +1,35 @@
-let currentIndex = 0;
-let textElement = document.getElementById("typed-text");
-let colorOverlay = document.getElementById("color-overlay");
-let cursor = document.getElementById("typed-cursor");
-let heroDesc = document.querySelector(".hero-desc");
+"use strict";
 
-let titleData = [
-  {
-    id: 0,
-    title: "Software engineer?",
-    backgroundColor: "rgba(40, 238, 167, 0.93)",
-    textColor: "#363738",
-  },
-  {
-    id: 1,
-    title: "Data engineer?",
-    backgroundColor: "rgba(152, 100, 218, 0.93)",
-    textColor: "#ffffff",
-  },
-  {
-    id: 2,
-    title: "UI/UX designer?",
-    backgroundColor: "rgba(251, 80, 142, 0.93)",
-    textColor: "#ffffff",
-  },
-];
+var currentIndex = 0;
+var textElement = document.getElementById("typed-text");
+var colorOverlay = document.getElementById("color-overlay");
+var cursor = document.getElementById("typed-cursor");
+var heroDesc = document.querySelector(".hero-desc");
+var titleData = [{
+  id: 0,
+  title: "Software engineer?",
+  backgroundColor: "rgba(40, 238, 167, 0.93)",
+  textColor: "#363738"
+}, {
+  id: 1,
+  title: "Data engineer?",
+  backgroundColor: "rgba(152, 100, 218, 0.93)",
+  textColor: "#ffffff"
+}, {
+  id: 2,
+  title: "UI/UX designer?",
+  backgroundColor: "rgba(251, 80, 142, 0.93)",
+  textColor: "#ffffff"
+}];
 
 function typeText() {
-  let currentTitleData = titleData[currentIndex];
-
+  var currentTitleData = titleData[currentIndex];
   textElement.innerHTML = "";
   textElement.style.color = currentTitleData.textColor;
   colorOverlay.style.backgroundColor = currentTitleData.backgroundColor;
   cursor.style.color = currentTitleData.textColor;
   heroDesc.style.color = currentTitleData.textColor;
-
-  let i = 0;
+  var i = 0;
 
   function typeCharacter() {
     if (i < currentTitleData.title.length) {
@@ -63,31 +58,25 @@ function typeText() {
 }
 
 typeText();
-
-const menuIcon = document.querySelector(".menu-icon");
-const closeIcon = document.querySelector(".close-icon");
-const navMenu = document.querySelector(".nav-menu");
-const menuItems = document.querySelectorAll(".menu-item");
-
+var menuIcon = document.querySelector(".menu-icon");
+var closeIcon = document.querySelector(".close-icon");
+var navMenu = document.querySelector(".nav-menu");
+var menuItems = document.querySelectorAll(".menu-item");
 menuIcon.addEventListener("click", function () {
   toggleMenu();
 });
-
 closeIcon.addEventListener("click", function () {
   toggleMenu();
 });
-
 menuItems.forEach(function (menuItem) {
   menuItem.addEventListener("click", function () {
     // Remove 'current-item' class from all menu items
     menuItems.forEach(function (item) {
       item.classList.remove("current-item");
-    });
+    }); // Add 'current-item' class to the clicked menu item
 
-    // Add 'current-item' class to the clicked menu item
-    menuItem.classList.add("current-item");
+    menuItem.classList.add("current-item"); // Close the menu
 
-    // Close the menu
     toggleMenu();
   });
 });
@@ -105,27 +94,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function openTab(tabName) {
   // Hide all tabs
-  const tabs = document.querySelectorAll(".tab-content");
+  var tabs = document.querySelectorAll(".tab-content");
   tabs.forEach(function (tab) {
     tab.style.display = "none";
-  });
+  }); // Show the selected tab
 
-  // Show the selected tab
-  const selectedTab = document.getElementById(tabName);
+  var selectedTab = document.getElementById(tabName);
+
   if (selectedTab) {
     selectedTab.style.display = "block";
-  }
+  } // Change the background color based on the selected tab
 
-  // Change the background color based on the selected tab
-  const container = document.getElementById("programs");
-  const tabButtons = document.querySelectorAll(".tab-name");
-  const tabContainer = document.querySelectorAll(".program-container-parent");
 
-  // Remove "active" class from all tabs and buttons
+  var container = document.getElementById("programs");
+  var tabButtons = document.querySelectorAll(".tab-name");
+  var tabContainer = document.querySelectorAll(".program-container-parent"); // Remove "active" class from all tabs and buttons
+
   tabContainer.forEach(function (tab) {
     tab.classList.remove("active");
   });
-
   tabButtons.forEach(function (button) {
     button.classList.remove("active");
     button.style.color = "#373839";
@@ -134,42 +121,47 @@ function openTab(tabName) {
   switch (tabName) {
     case "tab1":
       container.style.backgroundColor = "rgb(255, 198, 53)";
-      tabButtons.forEach(
-        (button) => (button.style.color = "rgb(255, 198, 53)")
-      );
+      tabButtons.forEach(function (button) {
+        return button.style.color = "rgb(255, 198, 53)";
+      });
       tabButtons[0].classList.add("active");
       tabContainer[0].classList.add("active");
       break;
+
     case "tab2":
       container.style.backgroundColor = "rgb(40, 238, 167)";
-      tabButtons.forEach(
-        (button) => (button.style.color = "rgb(40, 238, 167)")
-      );
+      tabButtons.forEach(function (button) {
+        return button.style.color = "rgb(40, 238, 167)";
+      });
       tabButtons[1].classList.add("active");
       tabContainer[1].classList.add("active");
       break;
+
     case "tab3":
       container.style.backgroundColor = "rgb(152, 100, 218)";
-      tabButtons.forEach(
-        (button) => (button.style.color = "rgb(152, 100, 218)")
-      );
+      tabButtons.forEach(function (button) {
+        return button.style.color = "rgb(152, 100, 218)";
+      });
       tabButtons[2].classList.add("active");
       tabContainer[2].classList.add("active");
       break;
+
     case "tab4":
       container.style.backgroundColor = "rgb(251, 80, 142)";
-      tabButtons.forEach(
-        (button) => (button.style.color = "rgb(251, 80, 142)")
-      );
+      tabButtons.forEach(function (button) {
+        return button.style.color = "rgb(251, 80, 142)";
+      });
       tabButtons[3].classList.add("active");
       tabContainer[3].classList.add("active");
       break;
+
     default:
       console.log(tabButtons);
       container.style.backgroundColor = "rgb(255, 198, 53)"; // Default color when no tab is selected
-      tabButtons.forEach(
-        (button) => (button.style.color = "rgb(255, 198, 53)")
-      );
+
+      tabButtons.forEach(function (button) {
+        return button.style.color = "rgb(255, 198, 53)";
+      });
       tabButtons[0].classList.add("active");
       tabContainer[0].classList.add("active");
   }
@@ -179,7 +171,6 @@ document.addEventListener("DOMContentLoaded", function () {
   var slider = document.querySelector(".slider-mask");
   var slides = document.querySelectorAll(".slide");
   var dotsContainer = document.querySelector(".dots-container");
-
   var currentSlide = 0;
 
   function showSlide(index) {
@@ -210,28 +201,24 @@ document.addEventListener("DOMContentLoaded", function () {
     for (var i = 0; i < slides.length; i++) {
       var dot = document.createElement("div");
       dot.classList.add("dot");
-      dot.addEventListener(
-        "click",
-        (function (index) {
-          return function () {
-            showSlide(index);
-          };
-        })(i)
-      );
+      dot.addEventListener("click", function (index) {
+        return function () {
+          showSlide(index);
+        };
+      }(i));
       dotsContainer.appendChild(dot);
     }
+
     updateDots();
   }
 
   createDots();
-
   var intervalId = setInterval(showNextSlide, 5000);
-
   slider.addEventListener("mouseenter", function () {
     clearInterval(intervalId);
   });
-
   slider.addEventListener("mouseleave", function () {
     intervalId = setInterval(showNextSlide, 5000);
   });
 });
+//# sourceMappingURL=index.dev.js.map
